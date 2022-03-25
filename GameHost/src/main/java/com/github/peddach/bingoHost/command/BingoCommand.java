7a70036b31cf76940fb18155691d3956013b1270
ProfessorSam <Samderprofessor@gmail.com>
2022-03-25 15:53:57 +0100

    Option to save all Blocks to a config

--- GameHost/src/main/java/com/github/peddach/bingoHost/command/BingoCommand.java
+++ GameHost/src/main/java/com/github/peddach/bingoHost/command/BingoCommand.java
@@ -1,10 +1,19 @@
 package com.github.peddach.bingoHost.command;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
+import com.github.peddach.bingoHost.GeneralSettings;
 import com.github.peddach.bingoHost.arena.Arena;
 import com.github.peddach.bingoHost.util.MessageUtil;
 
@@ -30,7 +39,38 @@ public class BingoCommand implements CommandExecutor {
 				}
 			}
 		}
+		if(args.length == 1 && args[0].equalsIgnoreCase("BlocksToYML")) {
+	        File blockList;
+	        FileConfiguration blockListConfig;
+	        blockList = new File(GeneralSettings.plugin.getDataFolder(), "blocks.yml");
+	        if (!blockList.exists()) {
+	            blockList.getParentFile().mkdirs();
+	            GeneralSettings.plugin.saveResource("blocks.yml", false);
+	         }
+
+	        blockListConfig = new YamlConfiguration();
+	        try {
+	            blockListConfig.load(blockList);
+	        } catch (IOException | InvalidConfigurationException e) {
+	            e.printStackTrace();
+	        }
+	        
+			ArrayList<String> list = new ArrayList<>();
+			for(Material mat : Material.values()) {
+				list.add(mat.name());
+			}
+			
+			blockListConfig.set("Blocks", list);
+			try {
+				blockListConfig.save(blockList);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			player.sendMessage("Blocks to yml sucess");
+		}
 		return false;
+
 	}
 
 }
