70dcd6ee9974367f7bc6e1fec9a8b4764e55e3b5
ProfessorSam <Samderprofessor@gmail.com>
2022-03-29 18:27:46 +0200

    Add Component to sendMessage()

--- GameHost/src/main/java/com/github/peddach/bingoHost/util/MessageUtil.java
+++ GameHost/src/main/java/com/github/peddach/bingoHost/util/MessageUtil.java
@@ -1,7 +1,14 @@
 package com.github.peddach.bingoHost.util;
 
+import java.time.Duration;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.title.Title;
+import net.kyori.adventure.title.Title.Times;
 
 public class MessageUtil {
 	public static void sendMessage(Player player, String message) {
@@ -14,5 +21,14 @@ public class MessageUtil {
 		}
 		message = ChatColor.translateAlternateColorCodes('&', message);
 		player.sendMessage(message);
+	}
+	
+	public static void sendMessage(Player player, Component component) {
+		Component prefix = Component.text("[").color(NamedTextColor.GRAY).append(Component.text("Bingo").color(NamedTextColor.GOLD).append(Component.text("] ").color(NamedTextColor.GRAY)));
+		player.sendMessage(prefix.append(component));
+	}
+	
+	public static Title titlebuilder(String title, String subtitle, int fadein, int stay, int fadeout) {
+		return Title.title(Component.text(title, NamedTextColor.GOLD), Component.text(subtitle, NamedTextColor.GRAY), Times.times(Duration.ofMillis(fadein), Duration.ofMillis(stay), Duration.ofMillis(fadeout)));
 	}
 }
