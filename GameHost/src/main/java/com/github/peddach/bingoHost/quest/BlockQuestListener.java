a8ed48ae36353d7c8f321ef69e0a35fc3e0b153a
ProfessorSam <Samderprofessor@gmail.com>
2022-04-17 00:03:06 +0200

    Only right or left click

--- GameHost/src/main/java/com/github/peddach/bingoHost/quest/BlockQuestListener.java
+++ GameHost/src/main/java/com/github/peddach/bingoHost/quest/BlockQuestListener.java
@@ -3,66 +3,24 @@ package com.github.peddach.bingoHost.quest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import com.github.peddach.bingoHost.arena.Arena;
 import com.github.peddach.bingoHost.arena.BingoTeam;
 import com.github.peddach.bingoHost.arena.GameState;
-import com.github.peddach.bingoHost.util.MessageUtil;
 import com.github.peddach.bingoHost.utilItems.BingoCard;
 
 public class BlockQuestListener implements Listener {
-
+		
 	@EventHandler
-	public void onPickUp(EntityPickupItemEvent event) {
-		if (event.getEntity()instanceof Player player) {
-			if (checkIfItemIsAQuest(player, event.getItem().getItemStack())) {
-				ItemStack item = event.getItem().getItemStack();
-				item.setAmount(item.getAmount() - 1);
-				if(item.getAmount() == 0) {
-					event.setCancelled(true);
-					event.getItem().remove();
-					return;
-				}
-				event.getItem().setItemStack(item);
-			}
-		}
-	}
-
-	@EventHandler
-	public void itemClickEvent(InventoryClickEvent event) {
-		if (QuestGui.getGuis().contains(event.getInventory())) {
+	public void onPlayerInteractEvent(PlayerInteractEvent event) {
+		if(!(event.getAction().isLeftClick() || event.getAction().isRightClick())) {
 			return;
 		}
-		if(event.getInventory().getType() == InventoryType.CRAFTING || event.getInventory().getType() == InventoryType.WORKBENCH) {
-			return;
-		}
-		if (checkIfItemIsAQuest((Player) event.getWhoClicked(), event.getCurrentItem())) {
-			event.getCurrentItem().setAmount(event.getCurrentItem().getAmount() - 1);
-			MessageUtil.sendMessage((Player) event.getWhoClicked(), "ClickEvent");
-		}
-	}
-
-	@EventHandler
-	public void onCraftEvent(CraftItemEvent event) {
-		if (!checkIfItemIsAQuest((Player) event.getWhoClicked(), event.getInventory().getResult())) {
-			return;
-		}
-		if(event.getClickedInventory() == null) {
-			return;
-		}
-		event.getClickedInventory().getItem(0).setAmount(event.getClickedInventory().getItem(0).getAmount() - 1);
-	}
-
-	@EventHandler
-	public void onMilkEvent(PlayerInteractEvent event) {
 		if (checkIfItemIsAQuest(event.getPlayer(), event.getItem())) {
 			event.getItem().setAmount(event.getItem().getAmount() - 1);
+			event.setCancelled(true);
 		}
 	}
 
