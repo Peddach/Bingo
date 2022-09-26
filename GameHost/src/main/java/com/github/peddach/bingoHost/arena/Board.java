9d7094b02e4ae42abfd5519e3a1a2b65f5927c6d
ProfessorSam <Samderprofessor@gmail.com>
2022-09-27 01:36:27 +0200

    Add stats

--- GameHost/src/main/java/com/github/peddach/bingoHost/arena/Board.java
+++ GameHost/src/main/java/com/github/peddach/bingoHost/arena/Board.java
@@ -1,6 +1,7 @@
 package com.github.peddach.bingoHost.arena;
 
 import com.github.peddach.bingoHost.GeneralSettings;
+import de.petropia.turtleServer.server.TurtleServer;
 import net.kyori.adventure.audience.Audience;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -14,18 +15,18 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 
+import java.util.Arrays;
+
 public class Board {
 	
-	private boolean quests[] = new boolean[25];
-	private Quest quest[] = new Quest[25]; 
-	private BingoTeam team;
+	private final boolean[] quests = new boolean[25];
+	private Quest[] quest = new Quest[25];
+	private final BingoTeam team;
 	
 	public Board(Quest[] quest, BingoTeam team) {
 		this.team = team;
 		this.quest = quest;
-		for(int i = 0; i < quests.length; i++) {
-			quests[i] = false;
-		}
+		Arrays.fill(quests, false);
 	}
 	
 	public void setSucess(int number) {
@@ -34,63 +35,84 @@ public class Board {
 			successMessage(quest[number].getBlock());
 		}
 		update();
+		for (Player player : team.getMembers()) {
+			if(player == null){
+				continue;
+			}
+			TurtleServer.getMongoDBHandler().getPetropiaPlayerByOnlinePlayer(player).thenAccept(petropiaPlayer -> {
+				petropiaPlayer.increateStats("Bingo_Points", 1);
+				petropiaPlayer.increateStats("Bingo_Quests", 1);
+			});
+		}
 	}
 	
 	public void update() {
 		//Horizontal
 		if(quests[0] && quests[1] && quests[2] && quests[3] && quests[4]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[5] && quests[6] && quests[7] && quests[8] && quests[9]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[10] && quests[11] && quests[12] && quests[13] && quests[14]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[15] && quests[16] && quests[17] && quests[18] && quests[19]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[20] && quests[21] && quests[22] && quests[23] && quests[24]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		//Vertically
 		if(quests[0] && quests[5] && quests[10] && quests[15] && quests[20]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[1] && quests[6] && quests[11] && quests[16] && quests[21]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[2] && quests[7] && quests[12] && quests[17] && quests[22]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[3] && quests[8] && quests[13] && quests[18] && quests[23]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[4] && quests[9] && quests[14] && quests[19] && quests[24]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		
 		//Cross
 		if(quests[0] && quests[6] && quests[12] && quests[18] && quests[24]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 		if(quests[4] && quests[8] && quests[12] && quests[16] && quests[20]) {
-			team.getArena().setWinner(team);
+			win();
 			return;
 		}
 	}
-	
+
+	private void win() {
+		team.getArena().setWinner(team);
+		for(Player player : team.getMembers()) {
+			if(player == null){
+				continue;
+			}
+			TurtleServer.getMongoDBHandler().getPetropiaPlayerByOnlinePlayer(player).thenAccept(petropiaPlayer -> {
+				petropiaPlayer.increateStats("Bingo_Wins", 1);
+				petropiaPlayer.increateStats("Bingo_Points", 5);
+			});
+		}
+	}
 	private void successMessage(Material block) {
 		Component blockname = Component.translatable(block).color(TextColor.color(255, 255, 50));
 		TextColor teamcolor = TextColor.fromCSSHexString(TeamUtil.teamMappingsNamedTextColor.get(team.getNumber()));
@@ -101,7 +123,7 @@ public class Board {
 			}
 			player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1F, 1F);
 		}
-		GeneralSettings.plugin.getMessageUtil().broadcastMessage(Audience.audience(team.getArena().getPlayers()), message);
+		GeneralSettings.plugin.getMessageUtil().sendMessage(Audience.audience(team.getArena().getPlayers()), message);
 	}
 
 	public Quest[] getQuest() {
