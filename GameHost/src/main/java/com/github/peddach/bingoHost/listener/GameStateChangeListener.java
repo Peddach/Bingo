87f598425ce483d9d07b90dc598c783eb66b72d2
ProfessorSam <76458774+ProfessorSam@users.noreply.github.com>
2023-01-07 19:53:53 +0100

    Iron Pickaxe on start

--- GameHost/src/main/java/com/github/peddach/bingoHost/listener/GameStateChangeListener.java
+++ GameHost/src/main/java/com/github/peddach/bingoHost/listener/GameStateChangeListener.java
@@ -29,10 +29,11 @@ public class GameStateChangeListener implements Listener {
 		if (event.getAfter() == GameState.INGAME) {
 			for (Player player : event.getArena().getPlayers()) {
 				player.getInventory().setItem(0, new ItemStack(Material.BREAD, 15));
+				player.getInventory().setItem(1, new ItemStack(Material.IRON_PICKAXE, 1));
 				player.getInventory().setItem(8, BingoCard.getItem());
 				player.getInventory().setItem(7, BackpackItem.getItem());
 				player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 250, false, false));
-				player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 20*60*3, 1, false, false));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 20*60*5, 1, false, false));
 			}
 			event.getArena().schedulePvpEnable();
 			new TraderSpawner(event.getArena());
